76de4f213e96d3c8070265437011c2f0aa697414
lkwoung <whrjzm123@gmail.com>
2023-03-08 14:02:17 +0900

    entity manager, transaction manager config 추가

--- movie/src/main/java/lkwoung/movie/config/DataSourceConfig.java
+++ movie/src/main/java/lkwoung/movie/config/DataSourceConfig.java
@@ -2,12 +2,18 @@ package lkwoung.movie.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -30,6 +36,25 @@ public class DataSourceConfig {
         dataSource.setPassword("root3306");
 
         return new HikariDataSource(dataSource);
+    }
+
+    @Primary
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("dataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("lkwoung.movie.entity")
+                .persistenceUnit("movieDB")
+                .build();
+    }
+
+    @Primary
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
     }
 
 }
