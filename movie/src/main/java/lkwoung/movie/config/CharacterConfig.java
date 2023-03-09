e87c09c809eca1f4bea73242283627f81e316be5
lkwoung <whrjzm123@gmail.com>
2023-03-09 21:33:47 +0900

    character config 추가

---
+++ movie/src/main/java/lkwoung/movie/config/CharacterConfig.java
@@ -0,0 +1,18 @@
+package lkwoung.movie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+public class CharacterConfig {
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+
+}
